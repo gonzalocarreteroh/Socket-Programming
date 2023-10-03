01fcd37abdaa7bf2c960e8549e389d2a9c5c3e23
JIAHAO CHEN <j2435che@uwaterloo.ca>
2023-10-03 18:15:16 -0400

    MM1

--- MM1.py
+++ MM1.py
@@ -4,10 +4,12 @@ import queue
 
 # Define simulation time (T) and arrival rate (lambda)
 T = 1000  # Adjust the simulation time as needed
-arrival_rate = 10.0  # Packets per second
+arrival_rate = 400.0  # Packets per second
 
 # Define service rate (mu)
-service_rate = 500.0  # Packets per second
+average_package_length = 2000  # Packets per second
+
+transmission_rate = 1000000 # bits per second
 
 # For M/M/1/K queue, define the buffer size (K)
 K = None  # Set to None for M/M/1 queue, or a positive integer for M/M/1/K queue
@@ -16,7 +18,6 @@ K = None  # Set to None for M/M/1 queue, or a positive integer for M/M/1/K queue
 clock = 0
 
 packet_counter = 0  # Counter to keep track of packet arrivals
-departure_time = math.inf  # Initialize departure time to infinity (no packet in service)
 
 arrival_events = queue.Queue() # List to store arrival events
 
@@ -50,13 +51,18 @@ clock = 0
 
 while not arrival_events.empty():
 
+    #for each arrival event, generate a departure event
     curr_event = arrival_events.get()
 
+    # departure time cannot be less than arrival time
     if clock < curr_event["time"]:
         clock = curr_event["time"]
 
-    service_time = generate_exponential(service_rate)
+    package_length = generate_exponential(1/average_package_length)
 
+    service_time = package_length/transmission_rate
+
+    #departure_time of next packat start after the end of the current packet
     clock += service_time
 
     packet = {
@@ -70,27 +76,26 @@ while not arrival_events.empty():
 
 clock = 0
 # Generate observer events
-observer_lambda = 5 * arrival_rate  # Adjust the rate of observer events as needed
+observer_average = 5 * arrival_rate  # Adjust the rate of observer events as needed
 while clock < T:
-    observer_time = generate_exponential(observer_lambda)
+    observer_time = generate_exponential(observer_average)
     clock += observer_time
 
     events.append({
         "type": "observer",
         'time': clock
     })
-
 events.sort(key = lambda x: x["time"])
 
 Na = 0 #Number of Arrivals
 Nd = 0 #Number of departures
 No = 0 #Number of observers
 
-Pidle = 0
+total_idle_time = 0
 sum_queue_length_observed = 0
 
 prev_idle = 0
-prev_time = None
+prev_idle_time = 0
 
 for event in events:
     if event["type"] == "arrival":
@@ -99,19 +104,22 @@ for event in events:
         Nd += 1
     else:
         if Na == Nd:
+            #server is idle
             if prev_idle == 1:
-                Pidle += event["time"] - prev_time
+                total_idle_time += event["time"] - prev_idle_time
+            else:
+                total_idle_time += (event["time"] - prev_idle_time) / 2
             prev_idle = 1
-            prev_time = event["time"]
+            prev_idle_time = event["time"]
         else:
             prev_idle = 1
-            prev_time = event["time"]
+            prev_idle_time = event["time"]
 
-        sum_queue_length_observed += (Na-Nd)
+        sum_queue_length_observed += abs(Na-Nd)
         No += 1
 
 average_queue_length = sum_queue_length_observed / No
-idle_ratio = Pidle / T
+idle_ratio = total_idle_time / T
 
 print(f"Average Queue Length: {average_queue_length}")
 print(f"Idle Ratio: {idle_ratio}")
