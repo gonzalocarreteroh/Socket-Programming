faea994c7a731ed20f47d9ce6c89fb03003e9451
Gonzalo Carretero <109541536+gonzalocarreteroh@users.noreply.github.com>
2023-10-06 04:41:33 +0200

    Update MM1K.py

--- MM1K.py
+++ MM1K.py
@@ -1,7 +1,6 @@
 import heapq
 import random
 import math
-import queue
 
 class MM1KQueueSimulator:
     def __init__(self, T, arrival_rate, average_package_length, transmission_rate, K=None):
@@ -12,7 +11,6 @@ class MM1KQueueSimulator:
         self.K = K
 
         self.clock = 0
-        self.arrival_events = queue.Queue()
         self.events = []
 
     def generate_exponential(self, lambda_param):
@@ -25,46 +23,12 @@ class MM1KQueueSimulator:
             self.clock += interarrival_time
 
             packet = (self.clock, "arrival")
-            """
-            packet = {
-                'type': "arrival",
-                'time': self.clock
-            }
-            """
-
             heapq.heappush(self.events, packet)
-            # self.arrival_events.put(packet)
-    
-    """
-    def generate_departures(self):
-
-        self.clock = 0
-
-        while not self.arrival_events.empty():
-            curr_event = self.arrival_events.get()
-
-            if self.clock < curr_event["time"]:
-                self.clock = curr_event["time"]
-
-            package_length = self.generate_exponential(1 / self.average_package_length)
-            service_time = package_length / self.transmission_rate
-
-            self.clock += service_time
-
-            packet = {
-                "type": "departure",
-                'time': self.clock
-            }
-
-            self.events.append(packet)
-    """
 
     def generate_service_time(self):
         package_length = self.generate_exponential(1 / self.average_package_length)
         service_time = package_length / self.transmission_rate
         return service_time
-
-
 
     def generate_observers(self):
 
@@ -81,21 +45,16 @@ class MM1KQueueSimulator:
     def run_simulation(self):
         self.generate_packet_arrivals()
         total_packets = len(self.events)
-        # self.generate_departures()
+
         self.generate_observers()
-        # self.events.sort(key = lambda x: x["time"])
 
         Na = 0  # Number of Arrivals
         Nd = 0  # Number of departures
         No = 0  # Number of observers
 
-        total_idle_time = 0
+        total_idle_count = 0
         sum_queue_length_observed = 0
         dropped_counter = 0
-
-        prev_idle = 0
-        prev_idle_time = 0
-
         prev_depart_time = 0
 
         while self.events:
@@ -124,32 +83,23 @@ class MM1KQueueSimulator:
             else:
                 if Na == Nd:
                     # server is idle
-                    if prev_idle == 1:
-                        total_idle_time += event[0] - prev_idle_time
-                    else:
-                        total_idle_time += (event[0] - prev_idle_time) / 2
-                    prev_idle = 1
-                    prev_idle_time = event[0]
-                else:
-                    prev_idle = 1
-                    prev_idle_time = event[0]
+                    total_idle_count += 1
 
                 sum_queue_length_observed += abs(Na - Nd)
                 No += 1
 
         average_queue_length = sum_queue_length_observed / No
-        idle_ratio = total_idle_time / T
+        idle_ratio = total_idle_count / No
         packet_loss_probability = dropped_counter / total_packets
 
         return average_queue_length, idle_ratio, packet_loss_probability
 
 # Usage example
-T = 1000
+T = 2000
 arrival_rate = 200.0
 average_package_length = 2000
 transmission_rate = 1000000
-K = 10
+K = 50
 
 simulator = MM1KQueueSimulator(T, arrival_rate, average_package_length, transmission_rate, K)
-# print(simulator.run_simulation())
-            
+print(simulator.run_simulation())
