26ce57d6d386cae4013e9c0d27eeafaa351f98fb
Gonzalo Carretero <109541536+gonzalocarreteroh@users.noreply.github.com>
2023-10-07 02:31:14 +0200

    Comments question1

--- question1.py
+++ question1.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 def generate_exponential_random_variables(lmbda, num_variables):
+    # Generate a list of uniform random variables between 0 and 1
     uniform_random_variables = np.random.uniform(0, 1, num_variables)
+    # Calculate exponential random variables using the inverse transform method
     exponential_random_variables = -(1 / lmbda) * np.log(1 - uniform_random_variables)
 
     return exponential_random_variables
