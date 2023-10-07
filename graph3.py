50fa24f182bc1b04b3786dacc4a2abd1486eeb10
Gonzalo Carretero <109541536+gonzalocarreteroh@users.noreply.github.com>
2023-10-07 02:25:53 +0200

    Comments graph3

--- graph3.py
+++ graph3.py
@@ -5,7 +5,7 @@ rhos = [0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]
 average_packet_counts1 = []
 average_packet_counts2 = []
 average_packet_counts3 = []
-
+# Simulation time
 T = 1000
 average_packet_length = 2000
 transmission_rate = 1000000
@@ -14,16 +14,20 @@ K = 10
 print("MM1K Plot E[n] vs. rho")
 
 for rho in rhos:
+    # Compute arrival rate for given rho
     arrival_rate = rho * transmission_rate / average_packet_length
     simulator1 = MM1KQueueSimulator(T, arrival_rate, average_packet_length, transmission_rate, K)
+    # Get E[n] from first simulator
     e_n1 = simulator1.run_simulation()[0]
     average_packet_counts1.append(e_n1)
     print(f"rho = {rho}, K = 10, E[N] = {e_n1}")
     simulator2 = MM1KQueueSimulator(T, arrival_rate, average_packet_length, transmission_rate, K + 15)
+    # Get E[n] from second simulator
     e_n2 = simulator2.run_simulation()[0]
     average_packet_counts2.append(e_n2)
     print(f"rho = {rho}, K = 25, E[N] = {e_n2}")
     simulator3 = MM1KQueueSimulator(T, arrival_rate, average_packet_length, transmission_rate, K + 40)
+    # Get E[n] from third simulator
     e_n3 = simulator3.run_simulation()[0]
     average_packet_counts3.append(e_n3)
     print(f"rho = {rho}, K = 50, E[N] = {e_n3}")
