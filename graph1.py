93d4530cb2e330dc3a85980adfa88d16c8fbce20
Gonzalo Carretero <109541536+gonzalocarreteroh@users.noreply.github.com>
2023-10-07 02:23:23 +0200

    Comments graph1

--- graph1.py
+++ graph1.py
@@ -4,18 +4,20 @@ from MM1 import MM1QueueSimulator
 rhos = [0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95]
 average_packet_counts = []
 
-
+# Simulator time
 T = 1000
 average_packet_length = 2000
 transmission_rate = 1000000
-
+# Infinite queue
 K = None
 
 print("MM1 Plot E[N] vs. rho")
 
 for rho in rhos:
+    # Compute arrival rate for given rho
     arrival_rate = rho * transmission_rate / average_packet_length
     simulator = MM1QueueSimulator(T, arrival_rate, average_packet_length, transmission_rate, K)
+    # Get E[n] from current simulator
     e_n = simulator.run_simulation()[0]
     average_packet_counts.append(e_n)
     print("rho = " + str(rho) + ", E[N] = " + str(e_n))
